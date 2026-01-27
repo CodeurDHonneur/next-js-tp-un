d0b42d302a8458e46f02d97015f852e5c7b0725f
= <=>
2026-01-27 16:24:42 +0100

    import emptyArticlesSection

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,9 +1,14 @@
+import { EmptyArticlesSection } from '@/components/EmptyArticlesSection'
 import React from 'react'
 
 const ArticlesPage = () => {
+  
   return (
     <div className="">
+      <h2 className='text-4xl md:text-5xl font-extrabold text-blue-600 tracking-tight text-center mt-6'>Exploration technique</h2>
+      <p className='font-semibold text-2xl text-center'>L'univers du développement vous ouvre ses portes</p>
 
+      <EmptyArticlesSection />
     </div>
   )
 }
